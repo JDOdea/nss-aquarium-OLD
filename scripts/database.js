81cd2836f97387d1df063404582329c6ca8f3f3b
JD Fitzmartin <jondavidodea@gmail.com>
2023-05-18 17:26:58 -0500

    Added Frederick the fish

---
+++ scripts/database.js
@@ -0,0 +1,31 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+
+/* 
+    id - int
+    name - string
+    food - string
+    size - int
+    rarity - string
+
+*/
+const database = {
+    fish: [
+        {
+            id: 1,
+            name: "Bart",
+            food: "crustaceans",
+            size: 3
+        },
+        {
+            id: 2,
+            name: "Frederick",
+            food: "compliments",
+            size: 5
+        }
+    ]
+}
